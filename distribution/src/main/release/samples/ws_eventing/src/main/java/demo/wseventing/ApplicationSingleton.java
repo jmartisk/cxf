a435b21c9df4fdc6890c5aa2b4939f351592c596
Jan Martiska <jmartisk@redhat.com>
2013-02-07 16:09:11 +0100

    checkstyle rules compliance for demo app

--- distribution/src/main/release/samples/ws_eventing/src/main/java/demo/wseventing/ApplicationSingleton.java
+++ distribution/src/main/release/samples/ws_eventing/src/main/java/demo/wseventing/ApplicationSingleton.java
@@ -1,3 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package demo.wseventing;
 
 import java.util.ArrayList;
@@ -5,18 +24,19 @@ import java.util.List;
 
 import demo.wseventing.eventapi.CatastrophicEventSinkImpl;
 
-public class ApplicationSingleton {
+public final class ApplicationSingleton {
 
     private static ApplicationSingleton instance;
     private List<CatastrophicEventSinkImpl> eventSinks = new ArrayList<CatastrophicEventSinkImpl>();
 
-    public static ApplicationSingleton getInstance() {
-        if (instance == null)
-            instance = new ApplicationSingleton();
-        return instance;
+    private ApplicationSingleton() {
     }
 
-    private ApplicationSingleton() {
+    public static ApplicationSingleton getInstance() {
+        if (instance == null) {
+            instance = new ApplicationSingleton();
+        }
+        return instance;
     }
 
     public void createEventSink(String url) {
@@ -32,8 +52,9 @@ public class ApplicationSingleton {
 
     public CatastrophicEventSinkImpl getEventSinkByURL(String url) {
         for (CatastrophicEventSinkImpl eventSink : eventSinks) {
-            if (eventSink.getShortURL().equals(url))
+            if (eventSink.getShortURL().equals(url)) {
                 return eventSink;
+            }
         }
         return null;
     }
--- distribution/src/main/release/samples/ws_eventing/src/main/java/demo/wseventing/CreateSubscriptionServlet.java
+++ distribution/src/main/release/samples/ws_eventing/src/main/java/demo/wseventing/CreateSubscriptionServlet.java
@@ -1,12 +1,26 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package demo.wseventing;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,8 +32,6 @@ import javax.xml.bind.Marshaller;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
-
-import org.w3c.dom.Node;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
@@ -38,7 +50,7 @@ public class CreateSubscriptionServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+        throws ServletException, IOException {
         try {
             resp.getWriter().append("<html><body>");
 
@@ -67,7 +79,7 @@ public class CreateSubscriptionServlet extends HttpServlet {
     }
 
     public Subscribe createSubscribeMessage(String targetURL, String filter)
-            throws DatatypeConfigurationException {
+        throws DatatypeConfigurationException {
         Subscribe sub = new Subscribe();
 
         // expires
